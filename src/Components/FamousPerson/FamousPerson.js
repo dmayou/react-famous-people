6cf044018af2f4f542973cf5f22ec8b7f81dbb36
David Mayou <davidpmayou@gmail.com>
2018-11-27 16:30:54 -0600

    add name input

--- src/Components/FamousPerson/FamousPerson.js
+++ src/Components/FamousPerson/FamousPerson.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class FamousPerson extends Component {
-    state = {
-        name : '',
-        role : ''
+    state = { person: {
+                name : '',
+                role : ''
+            }
+    }
+    handleChange = (propertyName) => {
+        return ( (event) => {
+            console.log(`In handler: `, event.target.value);
+            this.setState({ person: {
+                    ...this.state.person, [propertyName] : event.target.value
+                }
+            })
+        });
     }
     render() {
         return (
             <div>
-                <p>Hello world!</p>
+                <input type="text" placeholder="name" onChange={ this.handleChange('name') } />
+                <p>Hello world! {this.state.person.name + ' ' + this.state.person.role}</p>
             </div>
         );
     }
