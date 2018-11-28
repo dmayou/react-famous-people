32147ef700c1c2668bd09711277e61666339bff1
David Mayou <davidpmayou@gmail.com>
2018-11-28 16:43:45 -0600

    separate state and event handlers to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,11 +5,40 @@ import Header from '../Header/Header';
 import FamousPerson from '../FamousPerson/FamousPerson';
 
 class App extends Component {
+  state = {
+    person: {
+      name: '',
+      role: ''
+    },
+    people: [
+      { name: 'firstPerson', role: 'firstRole' },
+      { name: 'secondPerson', role: 'secondPerson' }
+    ]
+  }
+  handleChange = (propertyName) => {
+    return ((event) => {
+      console.log(`In handler: `, event.target.value);
+      this.setState({
+        person: {
+          ...this.state.person, [propertyName]: event.target.value
+        }
+      })
+    });
+  }
+  handleClick = (event) => {
+    console.log('the values of state are:', this.state);
+    this.setState({ people: [...this.state.people, this.state.person] });
+  }
   render() {
     return (
       <div className="App">
         <Header />
-        <FamousPerson />
+        <FamousPerson
+          person={this.state.person}
+          people={this.state.people}
+          handleChange={this.handleChange}
+          handleClick={this.handleClick}
+           />
       </div>
     );
   }
--- src/Components/FamousPerson/FamousPerson.js
+++ src/Components/FamousPerson/FamousPerson.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
 
 class FamousPerson extends Component {
-    state = { person: {
-                name : '',
-                role : ''
-            },
-            people: [
-                {name:'firstPerson', role:'firstRole'},
-                {name:'secondPerson', role:'secondPerson'}
-            ]
-    }
-    handleChange = (propertyName) => {
-        return ( (event) => {
-            console.log(`In handler: `, event.target.value);
-            this.setState({ person: {
-                    ...this.state.person, [propertyName] : event.target.value
-                }
-            })
-        });
-    }
-    handleClick = (event) => {
-        console.log('the values of state are:', this.state);
-        this.setState({ people: [...this.state.people, this.state.person]});
-    }
+    // state = { person: {
+    //             name : '',
+    //             role : ''
+    //         },
+    //         people: [
+    //             {name:'firstPerson', role:'firstRole'},
+    //             {name:'secondPerson', role:'secondPerson'}
+    //         ]
+    // }
+    // handleChange = (propertyName) => {
+    //     return ( (event) => {
+    //         console.log(`In handler: `, event.target.value);
+    //         this.setState({ person: {
+    //                 ...this.props.person, [propertyName] : event.target.value
+    //             }
+    //         })
+    //     });
+    // }
+    // handleClick = (event) => {
+    //     console.log('the values of state are:', this.props);
+    //     this.setState({ people: [...this.props.people, this.props.person]});
+    // }
     render() {
-        let personList = this.state.people.map((person, i) => 
+        let personList = this.props.people.map((person, i) => 
             <li key={i}>{person.name} is famous for {person.role}</li>
         );
         return (
             <div>
-                <input type="text" placeholder="name" onChange={ this.handleChange('name') } />
-                <input type="text" placeholder="role" onChange={this.handleChange('role')} />
-                <button onClick={this.handleClick}>Log State to Console</button>
+                <input type="text" value={this.props.person.name} placeholder="name" onChange={ this.props.handleChange('name') } />
+                <input type="text" value={this.props.person.role} placeholder="role" onChange={ this.props.handleChange('role')} />
+                <button onClick={this.props.handleClick}>Log State to Console</button>
                 {personList}
             </div>
         );
